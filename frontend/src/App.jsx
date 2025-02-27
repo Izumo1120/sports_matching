15bef14c170b69c319e0f575952e056526824688
kei0101xw <kei0101xw@icloud.com>
2025-02-27 19:59:12 +0900

    ログイン/ログアウトできるようにした

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import Recruit from "./pages/Recruit";
 import Confirm from "./pages/Confirm";
 import Guest from "./pages/Guest";
 import Details from "./pages/Details";
 import ManagementEvent from "./pages/ManagementEvent";
 
-
 import Navbar from "./components/Navbar";
 
 function App() {
+  const [isAuth, setIsAuth] = useState(localStorage.getItem("isAuth"));
+
   return (
     <Router>
-      <Navbar />
+      <Navbar isAuth={isAuth} setIsAuth={setIsAuth} />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login setIsAuth={setIsAuth} />} />
         <Route path="/recruit" element={<Recruit />} />
         <Route path="/confirm" element={<Confirm />} />
         <Route path="/guest" element={<Guest />} />
