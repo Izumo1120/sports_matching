10012f1fc468ef09f8d370678f713b8cf793b9f1
kei0101xw <kei0101xw@icloud.com>
2025-02-27 23:44:22 +0900

    ナビゲーションバーにハンバーガーボタン追加（途中）

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,7 +7,6 @@ import "./Navbar.css"; // スタイル用のCSSファイルを作成
 //アイコン
 import LoginIcon from "@mui/icons-material/Login";
 import LogoutIcon from "@mui/icons-material/Logout";
-import PersonAddIcon from "@mui/icons-material/PersonAdd";
 import GroupAddIcon from "@mui/icons-material/GroupAdd";
 import ManageSearchIcon from "@mui/icons-material/ManageSearch";
 
@@ -22,39 +21,42 @@ const Navbar = ({ isAuth, setIsAuth }) => {
     });
   };
   return (
-    <nav className="navbar">
-      <div className="navbar-brand">
-        <Link to="/" className="navbar-title">
-          SAGAスポーツマッチング
-        </Link>
-      </div>
-      <div className="navbar-center"></div>
-      <div className="navbar-auth">
-        <Link to="/recruit" className="nav-link">
-          <GroupAddIcon />
-          募集する
-        </Link>
-        <Link to="/guest" className="nav-link">
-          <ManageSearchIcon />
-          参加する
-        </Link>
-        <Link to="/signup" className="nav-link">
-          <PersonAddIcon />
-          新規登録
-        </Link>
-        {!isAuth ? (
-          <Link to="/login" className="nav-link">
-            <LoginIcon />
-            ログイン
+    <div className="container">
+      <nav className="navbar">
+        <div className="navbar-brand">
+          <Link to="/" className="navbar-title">
+            SAGAスポーツマッチング
           </Link>
-        ) : (
-          <button className="nav-button" onClick={logout}>
-            <LogoutIcon />
-            ログアウト
-          </button>
-        )}
-      </div>
-    </nav>
+        </div>
+        <div className="navbar-center"></div>
+        <div className="navbar-auth">
+          <Link to="/recruit" className="nav-link">
+            <GroupAddIcon />
+            募集する
+          </Link>
+          <Link to="/guest" className="nav-link">
+            <ManageSearchIcon />
+            参加する
+          </Link>
+          {!isAuth ? (
+            <Link to="/login" className="nav-link">
+              <LoginIcon />
+              ログイン
+            </Link>
+          ) : (
+            <button className="nav-button" onClick={logout}>
+              <LogoutIcon />
+              ログアウト
+            </button>
+          )}
+          <div className="hamburger-container">
+            <div className="hamburger"></div>
+            <div className="hamburger"></div>
+            <div className="hamburger"></div>
+          </div>
+        </div>
+      </nav>
+    </div>
   );
 };
 
