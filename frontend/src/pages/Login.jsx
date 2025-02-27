4fd908afb0d12569ad39b43c98ecccb6c8ea15b2
shu-hub0525 <shugo.y0525@gmail.com>
2025-02-27 23:35:14 +0900

    ログイン画面（仮）

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import { signInWithPopup } from "firebase/auth";
 import { auth, provider } from "../firebase";
 import { useNavigate } from "react-router-dom";
+import { Box, Button, Typography, Container, Paper } from "@mui/material";
+import GoogleIcon from "@mui/icons-material/Google";  // Googleアイコンの使用
 
 const Login = ({ setIsAuth }) => {
   const navigate = useNavigate();
+
   const loginInWithGoogle = () => {
-    //Googleログイン
+    // Googleログイン
     signInWithPopup(auth, provider).then((result) => {
       localStorage.setItem("isAuth", true);
       setIsAuth(true);
@@ -15,10 +18,25 @@ const Login = ({ setIsAuth }) => {
   };
 
   return (
-    <div>
-      <p>ログインして始める</p>
-      <button onClick={loginInWithGoogle}>Googleでログイン</button>
-    </div>
+    <Container component="main" maxWidth="xs">
+      <Paper elevation={3} sx={{ padding: 4 }}>
+        <Box display="flex" flexDirection="column" alignItems="center">
+          <Typography variant="h5" component="h1" gutterBottom>
+            ログインして始める
+          </Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            fullWidth
+            startIcon={<GoogleIcon />}
+            onClick={loginInWithGoogle}
+            sx={{ marginTop: 2 }}
+          >
+            Googleでログイン
+          </Button>
+        </Box>
+      </Paper>
+    </Container>
   );
 };
 
