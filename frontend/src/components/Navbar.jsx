18356acad226dbea5edd479f6b488f5f58171791
shu-hub0525 <shugo.y0525@gmail.com>
2025-02-27 16:36:33 +0900

    ナビゲーションバー変更

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,12 @@
 import { Link } from "react-router-dom";
 import "./Navbar.css"; // スタイル用のCSSファイルを作成
 
+//アイコン
+import LoginIcon from '@mui/icons-material/Login';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import GroupAddIcon from '@mui/icons-material/GroupAdd';
+import ManageSearchIcon from '@mui/icons-material/ManageSearch';
+
 const Navbar = () => {
   return (
     <nav className="navbar">
@@ -12,15 +18,19 @@ const Navbar = () => {
       <div className="navbar-center"></div>
       <div className="navbar-auth">
         <Link to="/recruit" className="nav-link">
+          <GroupAddIcon />
           募集する
         </Link>
         <Link to="/guest" className="nav-link">
+          <ManageSearchIcon />
           参加する
         </Link>
         <Link to="/signup" className="nav-link">
+          <PersonAddIcon />
           新規登録
         </Link>
         <Link to="/login" className="nav-link">
+          <LoginIcon />
           ログイン
         </Link>
       </div>
